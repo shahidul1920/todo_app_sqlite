bcaf4066cb5b4b0599599dd9ef886e838f418706
Shahidul Islam <shahidul13@uttarauniversity.edu.bd>
2024-11-30 23:50:11 +0600

    bug fixing

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,5 @@
 import { Header } from "../component/Header"
 
-const metadata = {
-    title: "bla bla",
-    description: "bla bla",
-}
-
 const DashboardLayout = ({children}:{children: React.ReactNode}) => {
   return (
     <div>
